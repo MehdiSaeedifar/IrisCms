39d9acc0a339354d30d143fdcb5f3ce9e00c1b19
Mehdi Saeedifar <saeedifarmehdi@gmail.com>
2015-09-28 11:13:09 +0330

    add Entry method

--- Iris.Datalayer/Context/IUnitOfWork.cs
+++ Iris.Datalayer/Context/IUnitOfWork.cs
@@ -1,10 +1,12 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 
 namespace Iris.Datalayer.Context
 {
     public interface IUnitOfWork
     {
         IDbSet<TEntity> Set<TEntity>() where TEntity : class;
+        DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
         int SaveChanges();
     }
 }
